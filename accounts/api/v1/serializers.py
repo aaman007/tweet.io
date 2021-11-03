50d0fc4da1e0f95958f8af2aa25ea4bc07dae7a8
Amanur Rahman <aaman.sunny007@gmail.com>
2021-11-03 13:33:43 +0600

    Fixed issue with followers_count and follows_count being multiplied, added sql joins in tweet list api

--- accounts/api/v1/serializers.py
+++ accounts/api/v1/serializers.py
@@ -14,6 +14,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         return User.objects.create_user(**validated_data)
 
 
+class BasicUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'full_name']
+
+
 class UserSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField()
     follows_count = serializers.SerializerMethodField()
@@ -22,34 +28,28 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'full_name', 'followers_count', 'follows_count']
 
+    # noinspection PyMethodMayBeStatic
     def get_followers_count(self, obj: User):
         if hasattr(obj, 'followers_count'):
             return getattr(obj, 'followers_count')
         return obj.active_followers().count()
 
+    # noinspection PyMethodMayBeStatic
     def get_follows_count(self, obj: User):
         if hasattr(obj, 'follows_count'):
             return getattr(obj, 'follows_count')
         return obj.active_follows().count()
 
 
-class SingleUserSerializer(serializers.ModelSerializer):
+class SingleUserSerializer(UserSerializer):
     is_following = serializers.SerializerMethodField()
-    followers_count = serializers.SerializerMethodField()
-    follows_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['username', 'full_name', 'followers_count', 'follows_count', 'is_following']
+        fields = UserSerializer.Meta.fields + ['is_following']
 
     def get_is_following(self, obj: User):
         return self.context['request'].user.active_follows().filter(user=obj).exists()
-
-    def get_followers_count(self, obj: User):
-        return obj.active_followers().count()
-
-    def get_follows_count(self, obj: User):
-        return obj.active_follows().count()
 
 
 class FollowSerializer(serializers.ModelSerializer):
